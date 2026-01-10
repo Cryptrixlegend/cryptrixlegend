1776a8f6339010164e12fdcd2501b3dd6b342a9a
CRYPTRIXLEGEND <tanishbaliyan07@gmail.com>
2026-01-10 20:05:43 +0530

    Update snow.js

--- snow.js
+++ snow.js
@@ -1,46 +1,38 @@
-const canvas = document.createElement('canvas');
-canvas.id = 'snow-canvas';
-document.body.appendChild(canvas);
-const ctx = canvas.getContext('2d');
+const canvas=document.getElementById("snow");
+const ctx=canvas.getContext("2d");
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let snowflakes = [];
-
-function createSnowflakes() {
-  const x = Math.random() * canvas.width;
-  const y = Math.random() * canvas.height;
-  const radius = Math.random() * 3 + 1;
-  const speed = Math.random() * 1 + 0.5;
-  snowflakes.push({x, y, radius, speed});
-}
-
-for (let i = 0; i < 150; i++) createSnowflakes();
-
-function drawSnow() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = 'white';
-  ctx.beginPath();
-  snowflakes.forEach(s => {
-    ctx.moveTo(s.x, s.y);
-    ctx.arc(s.x, s.y, s.radius, 0, Math.PI * 2);
-  });
-  ctx.fill();
-  updateSnow();
-}
-
-function updateSnow() {
-  snowflakes.forEach(s => {
-    s.y += s.speed;
-    if (s.y > canvas.height) s.y = 0;
-  });
-  requestAnimationFrame(drawSnow);
-}
-
-drawSnow();
-window.addEventListener('resize', () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+let flakes=[];
+for(let i=0;i<100;i++){
+flakes.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+r:Math.random()*3+1,
+d:Math.random()+1
 });
-<script src="time.js"></script>
+}
+
+function draw(){
+ctx.clearRect(0,0,canvas.width,canvas.height);
+ctx.fillStyle="white";
+ctx.beginPath();
+for(let f of flakes){
+ctx.moveTo(f.x,f.y);
+ctx.arc(f.x,f.y,f.r,0,Math.PI*2,true);
+}
+ctx.fill();
+move();
+}
+
+function move(){
+for(let f of flakes){
+f.y+=f.d;
+if(f.y>canvas.height){
+f.y=0;
+f.x=Math.random()*canvas.width;
+}
+}
+}
+
+setInterval(draw,33);
