aefbd7fb3310300d81e17df57d0e40bf58045bfe
CRYPTRIXLEGEND <tanishbaliyan07@gmail.com>
2026-01-10 20:05:30 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,13 +1,3 @@
-window.addEventListener('load', () => {
-  const loader = document.getElementById('loader');
-
-  // 3 seconds delay
-  setTimeout(() => {
-    loader.style.opacity = 0; // start fade out
-
-    // After fade completes, remove loader from DOM
-    setTimeout(() => {
-      loader.style.display = 'none';
-    }, 500); // fade duration matches CSS transition
-  }, 3000); // 3 seconds
-});
+setTimeout(()=>{
+document.getElementById("loader").style.display="none";
+},3000);
